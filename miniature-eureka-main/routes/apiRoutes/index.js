824dea99dcb036656e30a137879b4d21d0090866
Andy MCA <AndrewMcAlpin@gmail.com>
2022-03-12 11:45:06 -0600

    api get route

--- miniature-eureka-main/routes/apiRoutes/index.js
+++ miniature-eureka-main/routes/apiRoutes/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
-const { db } = require('../../db/db');
+const { notesData } = require('../../db/db');
 
 router.get('/notes', (req, res) => {
-
+    let notes = notesData;
+    res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
+    req.body.id = db.length.toString();
 
 });
 
